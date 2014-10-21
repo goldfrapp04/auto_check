1d4c4a19f5d1bc229e3cf610b018955d65796b45
Yue Liu <Shia@Yues-MacBook-Air.local>
2014-10-21 16:30:21 -0400

    first commit

---
+++ event.js
@@ -0,0 +1,14 @@
+function injectedMethod (tab, method, callback) {
+  chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
+    chrome.tabs.sendMessage(tab.id, { method: method }, callback);
+  });
+}
+
+function autoCheck (tab) {
+  injectedMethod(tab, 'autoCheck', function (response) {
+    alert('Elements in tab: ' + response.data);
+    return true;
+  });
+}
+
+chrome.browserAction.onClicked.addListener(autoCheck);
